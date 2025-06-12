e8dbccd6ba5289f0c98cb6d7b565602e8c2f6818
khaled29-coder <khaledhussein957@gmail.com>
2025-06-12 10:21:14 +0300

    i updated the the owner approvel imported the model and check the id in the param is in the owner

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -1,19 +1,20 @@
 const User = require("../models/userModel");
+const Owner = require("../models/ownerModel");
 
 const ownerApproval = async (req, res) => {
     try {
-        const adminId = req.userId;
+        const adminId = req.user.id;
 
         const admin = await User.findById(adminId);
-        if (!admin) return res.status(400).json({ success: false, message: "Admin not found" });
+        if (!admin) return res.status(404).json({ success: false, message: "Admin not found" });
 
         if (admin.role !== "admin") return res.status(400).json({ success: false, message: "Unauthorized access" });
 
         const userId = req.params.id;
-        const owner = await Owner.findById(userId);
+        const owner = await Owner.findOne({ userId });
         if (!owner) return res.status(400).json({ success: false, message: "Owner not found" });
 
-        if (owner.is_verified === true) return res.status(400).json({ success: false, message: "Owner already approved" });
+        if (owner.isVerified === true) return res.status(400).json({ success: false, message: "Owner already approved" });
 
         owner.is_verified = true;
         await owner.save();
