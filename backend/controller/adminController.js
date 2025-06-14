cc1afa13cf933a9e1685e8646037e69fad8e57e4
khaled29-coder <khaledhussein957@gmail.com>
2025-06-14 11:43:03 +0300

    update the approveProperty and create the notification emails

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -1,5 +1,8 @@
 const Property = require("../models/propertyModel");
 const User = require("../models/userModel");
+const Owner = require("../models/ownerModel");
+
+const { sendApproveOwnerSuccess, sendApprovePropertySuccess } = require("../mail/emails");
 
 const approveProperty = async (req, res) => {
   try {
@@ -11,7 +14,7 @@ const approveProperty = async (req, res) => {
       return res.status(403).json({ message: "Access denied. Admins only." });
     }
 
-    const property = await Property.findById(propertyId);
+    const property = await Property.findById(propertyId).populate("owner", "userId"); // Populate the owner field and include userId
     if (!property) {
       return res.status(404).json({ message: "Property not found" });
     }
@@ -23,6 +26,14 @@ const approveProperty = async (req, res) => {
     property.isApproved = true;
     await property.save();
 
+    // check user role
+    const user = await User.findById(property.owner.userId); // Populate the owner field and include userId
+    if (!user || user.role !== "user") return res.status(400).json({ message: "Invalid user role" });
+
+
+    await sendApprovePropertySuccess(user.email);
+
+
     res.status(200).json({ message: "Property approved successfully", property });
 
   } catch (error) {
@@ -31,4 +42,36 @@ const approveProperty = async (req, res) => {
   }
 };
 
-module.exports = { approveProperty };
+const approveOwner = async (req, res) => {
+  try {
+    const adminId = req.userId;
+
+    const admin = await User.findById(adminId);
+    if (!admin) return res.status(404).json({ success: false, message: "Admin not found" });
+
+    if (admin.role !== "admin") return res.status(400).json({ success: false, message: "Unauthorized access" });
+
+    const userId = req.params.id;
+
+    const user = await User.findById(userId);
+    if (!user) return res.status(400).json({ success: false, message: "User not found" });
+
+    const owner = await Owner.findOne({ userId: user._id });
+    if (!owner) return res.status(400).json({ success: false, message: "Owner not found" });
+
+    if (owner.isVerified === true) return res.status(400).json({ success: false, message: "Owner already approved" });
+
+    owner.isVerified = true;
+    await owner.save();
+
+    await sendApproveOwnerSuccess(user.email);
+
+    res.status(200).json({ success: true, message: "Owner approved successfully" });
+
+  } catch (error) {
+    console.log("Error in ownerApproval ", error);
+    res.status(400).json({ success: false, message: "Internal server error" });
+  }
+};
+
+module.exports = { approveProperty, approveOwner };
--- backend/mail/emails.js
+++ backend/mail/emails.js
@@ -1,4 +1,4 @@
-const {mailtrapClient, sender} = require("./mailtrap.config");
+const { mailtrapClient, sender } = require("./mailtrap.config");
 
 
 const sendPasswordResetEmail = async (email, resetURL) => {
@@ -9,10 +9,10 @@ const sendPasswordResetEmail = async (email, resetURL) => {
 			from: sender,
 			to: recipient,
 			subject: "Reset your password",
-            text: `Your password reset link: ${resetURL}`,
+			text: `Your password reset link: ${resetURL}`,
 			category: "Password Reset",
 		});
-        
+
 	} catch (error) {
 		console.error("Error sending password reset email", error);
 
@@ -40,7 +40,49 @@ const sendResetSuccessEmail = async (email) => {
 	}
 };
 
+const sendApproveOwnerSuccess = async (email) => {
+	const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: "Owner Approved Successfully",
+			text: "Your Owner has been approved successfully",
+			category: "Owner Approval",
+		});
+
+		console.log("Password reset email sent successfully", response);
+	} catch (error) {
+		console.error("Error sending password reset success email", error);
+
+		throw new Error(`Error sending password reset success email: ${error}`);
+	}
+};
+
+const sendApprovePropertySuccess = async (email) => {
+	const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: "Property Approved Successfully",
+			text: "Your Property has been approved successfully",
+			category: "Property Approval",
+		});
+
+		console.log("Password reset email sent successfully", response);
+	} catch (error) {
+		console.error("Error sending password reset success email", error);
+
+		throw new Error(`Error sending password reset success email: ${error}`);
+	}
+};
+
 module.exports = {
-    sendPasswordResetEmail,
-    sendResetSuccessEmail
+	sendPasswordResetEmail,
+	sendResetSuccessEmail,
+	sendApproveOwnerSuccess,
+	sendApprovePropertySuccess
 }
