a3d4179b25114cb2a0ac7d9135fd86fa061b82f3
khaled29-coder <khaledhussein957@gmail.com>
2025-06-29 07:42:59 +0300

    create forgotPassword and resetPassword also i card ui component from shadcn, framer-motion as animation, react-hot-toast as alert

---
+++ frontend/src/pages/auth/ResetPassword.jsx
@@ -0,0 +1,80 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { resetPassword } from "@/services/api";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-hot-toast";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+
+function ResetPasswordPage() {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+
+  const { token } = useParams();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
+
+    resetPassword(token, password);
+    toast.success("Password reset successful");
+    setPassword("");
+    navigate("/login");
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <Card>
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl">Create New Password</CardTitle>
+          <CardDescription>Enter the new password below</CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="Password">Password</Label>
+            <Input
+              id="Password"
+              type="ypassword"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="Password">Confirm Password</Label>
+            <Input
+              id="confirmPassword"
+              type="confirmPassword"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full" onClick={handleSubmit}>
+            {isLoading ? "Loading..." : "Create a new Password"}
+          </Button>
+        </CardFooter>
+      </Card>
+    </motion.div>
+  );
+}
+
+export default ResetPasswordPage;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,28 @@
+/* eslint-disable no-useless-catch */
 import axios from 'axios';
 
 
 export const registerUser = async (userData) => {
     try {
         const response = await axios.post(`/api/users/register`, userData);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const forgotPassword = async (email) => {
+    try {
+        const response = await axios.post(`/api/users/forgot-password`, { email });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const resetPassword = async (token, password) => {
+    try {
+        const response = await axios.post(`/api/users/reset-password/${token}`, { password });
         return response.data;
     } catch (error) {
         throw error;
